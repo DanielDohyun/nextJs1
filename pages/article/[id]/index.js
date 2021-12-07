73a4599cdc008e2a8e9272fab41702b81cdb0f5e
Daniel Kim <kkdh1109@gmail.com>
2021-12-06 22:40:54 -0500

    fetching data using getStaticPaths

---
+++ pages/article/[id]/index.js
@@ -0,0 +1,65 @@
+import Link from "next/link";
+import { useRouter } from "next/router";
+
+const article = ({ article }) => {
+  //   const router = useRouter();
+  //   const { id } = router.query;
+
+  //   return <div>This is an article {id}</div>;
+  return (
+    <>
+      <h1>{article.title}</h1>
+      <p>{article.body}</p>
+      <br />
+      <Link href="/">Go Back</Link>
+      {/* This is an article {article.id} */}
+    </>
+  );
+};
+
+// export const getServerSideProps = async (context) => {
+//   const res = await fetch(
+//     `https://jsonplaceholder.typicode.com/posts/${context.params.id}`
+//   );
+
+//   const article = await res.json();
+
+//   return {
+//     props: {
+//       article,
+//     },
+//   };
+// };
+
+export const getStaticProps = async (context) => {
+  const res = await fetch(
+    `https://jsonplaceholder.typicode.com/posts/${context.params.id}`
+  );
+
+  const article = await res.json();
+
+  return {
+    props: {
+      article,
+    },
+  };
+};
+
+//requires ids and paths
+export const getStaticPaths = async () => {
+  const res = await fetch(`https://jsonplaceholder.typicode.com/posts`);
+
+  const articles = await res.json();
+
+  const ids = articles.map((article) => article.id);
+
+  const paths = ids.map((id) => ({ params: { id: id.toString() } }));
+
+  return {
+    paths,
+    //when we go to something that does not exist => return 404 page
+    fallback: false,
+  };
+};
+
+export default article;
