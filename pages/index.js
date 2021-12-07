d66bf7ea705fe3667b918ea65178109490cf4b2d
Daniel Kim <kkdh1109@gmail.com>
2021-12-06 22:12:21 -0500

    fetching data using getStaticProps

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,32 @@
 //used when you want custom title, meta-tags, keywords, desc, etc
 import Head from "next/head";
+import ArticleList from "../components/ArticleList";
 
 //lowercase for pages, and uppercase for components
 
-export default function Home() {
+export default function Home({ articles }) {
+  // console.log(articles);
+
   return (
     <div>
       <Head>
         <title>My first NextJS app </title>
         <meta name="keywords" content="NextJS" />
       </Head>
-      <h1>first nextJS app</h1>
+      <ArticleList articles={articles} />
     </div>
   );
 }
+
+export const getStaticProps = async () => {
+  const res = await fetch(
+    "https://jsonplaceholder.typicode.com/posts?_limit=6"
+  );
+  const articles = await res.json();
+
+  return {
+    props: {
+      articles,
+    },
+  };
+};
